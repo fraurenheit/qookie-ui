6093dd7b9208b038a4c8fbdefd169108f33b0dc0
fraurenheit <iremmbuyukcingil@gmail.com>
2022-09-25 22:28:09 +0300

    old one deleted and button project re-published.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
 
-import { ExampleComponent } from 'qookie-ui'
-import 'qookie-ui/dist/index.css'
+import { ExampleComponent, Button } from 'qookie-ui';
+import 'qookie-ui/dist/index.css';
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  return (
+  
+  <div style={{textAlign:'center'}  }>
+  <ExampleComponent text="Create React Library Example 😄" />
+  <Button type="primary" text="Primary Button"></Button> <br/>
+  <br></br>
+  <Button type="default" text="Default Button"></Button> <br/>
+  <br></br>
+  <Button type="dashed" text="Dashed Button"></Button> <br/>
+  <br></br>
+  <Button type="texty" text="Text Button"></Button> <br/>
+  <br></br>
+  <Button type="link" text="Link Button"></Button> <br/>
+  </div>
+
+)}
 
 export default App
